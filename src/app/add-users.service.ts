161bff01f892cdc09ad7cb1bbef28981bdb104e0
valliappan23 <86236266+valliappan23@users.noreply.github.com>
2022-04-28 09:35:06 +0530

    Update add-users.service.ts

--- src/app/add-users.service.ts
+++ src/app/add-users.service.ts
@@ -20,6 +20,8 @@ isLoggedIn(){
 }
 isEmployee(){
   let typeUsr = localStorage.loggeduserDesignation;
+  let typeUsrId = localStorage.loggeduser;
+  console.log(typeUsrId)
   
   let smTyp = typeUsr.toLowerCase();
   console.log(smTyp);
@@ -30,19 +32,65 @@ isEmployee(){
     return false;
   }
   else{
-    
-    // console.log("true varuthuuu");
+    //console.log("true varuthuuu");
+    if(smTyp==="manager"){
+      let a="employee";
+      console.log("FIND FOR EMPLOYEE");
+      return this.getManager();
+    }
     return true;
   }
 }
+getManager(){
+  return this.http.get<any[]>("http://localhost:3000/manager");
+}
+isMng(){
+  let typeUsr = localStorage.loggeduserDesignation;
+  let smTyp = typeUsr.toLowerCase();
+  console.log(smTyp);
+  if(smTyp === "manager"){
+    // console.log("falsee varuthuu");
+    return false;
+  }
+  else{
+    return true;
+  }
+  
+}
 
 
-getAllUsers(){
-  return this.http.get<any[]>("http://localhost:3000/allusers");
+isAdmin(){
+  let typeUsr = localStorage.loggeduserDesignation;
+  let smTyp = typeUsr.toLowerCase();
+  console.log(smTyp);
+  if(smTyp === "admin"){
+    // console.log("falsee varuthuu");
+    return false;
+  }
+  else{
+    return true;
+  }
+  
+}
+
+
+getAllUsers(userType:string){
+  return this.http.get<any[]>("http://localhost:3000/allusers/"+userType);
 }
 getSingleUsersData(userID:string){
+  console.log(userID," data need to fetch");
+  
   return this.http.get<any[]>("http://localhost:3000/singleusers/"+userID);
 }
+
+
+getUsersData(userID:string){
+  console.log(userID," data need to fetch");
+  
+  return this.http.get<any[]>("http://localhost:3000/single/"+userID);
+}
+
+
 editSingleUsersData(data:any){
   return this.http.put<string>("http://localhost:3000/EDITsingleusers",data);
 }
@@ -52,6 +100,34 @@ deleteUserData(UserId:number){
 searchUsers(searchtxt:string){
   return this.http.get<any[]>("http://localhost:3000/searchUser/"+searchtxt);
 }
+DoAddInAttendance(data:any){
+  return this.http.post<string>("http://localhost:3000/addINattendance",data);
+}
+getAllUsersInAttandance(){
+  return this.http.get<any[]>("http://localhost:3000/allusersInAttandance");
+}
+
+searchUsers1(searchtxt:string){
+  return this.http.get<any[]>("http://localhost:3000/searchUser1/"+searchtxt);
+}
+addAttendance(ID:string,data:string){
+  console.log()
+  const myData = {
+    ID,
+    data
+  }
+  return this.http.put<any>("http://localhost:3000/addAttendance/",myData);
+}
+resetAttendance(ID:string,data:string){
+  console.log()
+  const myData = {
+    ID,
+    data
+  }
+  return this.http.put<any>("http://localhost:3000/resetAttendance/",myData);
+}
+
+
 
 
 }
